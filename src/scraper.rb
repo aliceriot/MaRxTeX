7aa934d77b5bdad51dabc72759bfcc32870f2a26
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-10 19:44:02 -0400

    first draft of the scraper

---
+++ src/scraper.rb
@@ -0,0 +1,24 @@
+require 'mechanize'
+
+class Scraper
+  # take a url, return a nokogiri object
+  def initialize(url)
+    @url = url
+    @agent = Mechanize.new
+    @body = @agent.get(@url).search('body')
+    check_for_blockquote
+    delete_useless_children_from_body
+  end
+
+  def delete_useless_children_from_body
+    @body.keep_if { |node| ["p", "h3", "h2"].include? node.name }
+  end
+
+  def check_for_blockquote # some works are organized this way
+    if @body.children.map { |node| node.name }.include? "blockquote"
+      @body = @body.search('blockquote').children[1].children.to_a
+    else
+      @body = @body.children.to_a
+    end
+  end
+end
